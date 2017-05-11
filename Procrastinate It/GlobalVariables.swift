7899a9e413a4a046f79b4b0048ea2d0ecc50787c
Steven0351 <steven.sherry@affinityforapps.com>
2017-05-11 05:45:42 -0500

    Removed GoogleService-Info.plist to avoid potential security concerns and added it to .gitignore. Polished the 'Connect Account' section in SettingsTableVC and formatted the back button to go along with the app color scheme.

--- Procrastinate It/GlobalVariables.swift
+++ Procrastinate It/GlobalVariables.swift
@@ -7,13 +7,10 @@
 //
 
 import Foundation
-import FirebaseAuth
-import FirebaseDatabase
 
 typealias callback = (_ success: Bool) -> ()
 var didLogOut = false
 var facebookLoginSuccess = false
-//These are global so they can be created in the AuthService class and presented in another. 
-var firebaseAuthStatusCode: Int = 0
+//These are global so they can be created in the AuthService class and presented in another.
 var alert = AlertService.instance.customAlert(title: "Default", message: "This better not show up")
 
--- Procrastinate It/SettingsTableVC.swift
+++ Procrastinate It/SettingsTableVC.swift
@@ -20,6 +20,7 @@ class SettingsTableVC: UITableViewController {
     @IBOutlet weak var confirmPasswordTF: UITextField!
     @IBOutlet weak var connectAccountButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
+//    @IBOutlet weak var connectAccountSection: UITableViewSection!
     
     let user = FIRAuth.auth()!.currentUser!
     
@@ -28,6 +29,13 @@ class SettingsTableVC: UITableViewController {
         super.viewDidLoad()
         
         emailConversionForm.isHidden = true
+        if let navigationBar = self.navigationController?.navigationBar {
+            //Set back button color to compliment the app color scheme
+            navigationBar.tintColor = UIColor(red: 0.29, green: 0.65, blue: 0.65, alpha: 1.0)
+            if let topItem = navigationBar.topItem {
+                topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+            }
+        }
 
     }
 
@@ -42,6 +50,7 @@ class SettingsTableVC: UITableViewController {
     @IBAction func convertToFacebookTapped(_ sender: Any) {
         AuthService.instance.convertAnonToFacebook(user: user, viewController: self) { (success) in success ?
             self.dismiss(animated: true, completion: nil) : self.present(alert, animated: true, completion:  nil) }
+        
         }
     
     @IBAction func convertToEmailTapped(_ sender: Any) {
@@ -53,7 +62,13 @@ class SettingsTableVC: UITableViewController {
     @IBAction func connectAccountTapped(_ sender: Any) {
         AuthService.instance.convertAnonToEmail(user: user, email: emailTF.text!, password: passwordTF.text!, verifyPassword: confirmPasswordTF.text!) { (success) in success ? self.dismiss(animated: true, completion: nil) :
             self.present(alert, animated: true, completion:  nil) }
+        _ = navigationController?.popToRootViewController(animated: true)
     }
  
+    @IBAction func cancelTapped(_ sender: Any) {
+        emailConversionForm.isHidden = true
+        convertToEmailButton.isHidden = false
+        convertToFacebookButton.isHidden = false
+    }
 
 }
